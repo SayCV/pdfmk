4975b50b0dfaa7ff39e1b8e70843473eda7579e7
SayCV <sssxyc@163.com>
2022-12-16 18:03:23 +0800

    fix

--- pdf-toc.ts
+++ pdf-toc.ts
@@ -1,8 +1,11 @@
 import {
+  path,
+} from "./deps.ts";
+import {
   DOMParser,
   Element,
 } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
-import type { Config } from "./types.ts";
+import type { Config, PaperFormat } from "./types.ts";
 import { TocEntry } from "./types.ts";
 //import Buffer from "https://deno.land/x/buffer/mod.ts";
 import { Buffer } from "https://deno.land/std@0.162.0/node/buffer.ts";
@@ -13,7 +16,8 @@ import { createRequire } from "https://deno.land/std@0.103.0/node/module.ts";
 
 import parsePdf from "npm:pdfjs-parse@1";
 import { PDFDocument } from 'npm:pdf-lib@1';
-import { default as JSDOM } from 'npm:jsdom@19';
+// @deno-types=npm:jsdom@19/index.d.ts"
+import JSDOM from 'npm:jsdom@19';
 import UID from 'npm:uid-safe@2';
 import inlineCss from 'npm:inline-css@3';
 import handlebars from 'npm:handlebars@4';
@@ -30,7 +34,7 @@ function pageRender(pageData: any) {
   }
 
   return pageData.getTextContent(renderOptions).then((textContent: any) => {
-    let lastY,
+    let lastY: any,
       text = ''
     for (const item of textContent.items) {
       if (!lastY || lastY == item.transform[5]) {
@@ -86,46 +90,78 @@ export const extractPDFToc = async (pdfBuffer: Buffer, options: Config,
   tocContext: { _toc: TocEntry[] },
   context?: Record<string, unknown>,
   tocTemplate?: string): Promise<void> => {
-  const data = await parsePdf(pdfBuffer, { pagerender: pageRender, version: 'v2.0.550' })
-  data.text.split(PAGE_BREAK_MARKER).forEach((content: string, pageIndex: number) => {
-    tocContext._toc.map((entry) => {
-      if (content.includes(entry.id)) {
-        entry.page = pageIndex + 1
-      }
-      return entry
+    const data = await parsePdf(pdfBuffer, { pagerender: pageRender })
+    data.text.split(PAGE_BREAK_MARKER).forEach((content: string, pageIndex: number) => {
+      tocContext._toc.map((entry) => {
+        if (content.includes(entry.id)) {
+          entry.page = pageIndex + 1
+        }
+        return entry
+      })
     })
-  })
 
-  const document = new JSDOM(content).window.document
-  const tocElement: Element | null = document.querySelector('.print-toc')
-  document.querySelectorAll('.removeAfterTocExtraction').forEach((el: { parentNode: { removeChild: (arg0: any) => any; }; }) => el.parentNode?.removeChild(el))
-  if (tocElement) {
-    tocElement.innerHTML = (HandlebarsJS as any).compile(tocTemplate || '')({
-      ...context,
-      ...tocContext,
-    })
-    tocTemplate = tocElement.outerHTML
+    const document = new JSDOM(content).window.document
+    const tocElement: HTMLElement | null = document.querySelector('.print-toc')
+    document.querySelectorAll('.removeAfterTocExtraction').forEach((el: { parentNode: { removeChild: (arg0: any) => any; }; }) => el.parentNode?.removeChild(el))
+    if (tocElement) {
+      tocElement.innerHTML = handlebars.compile(tocTemplate || '')({
+        ...context,
+        ...tocContext,
+      })
+      tocTemplate = tocElement.outerHTML
+    }
+    content = document.documentElement.outerHTML
   }
-  content = document.documentElement?.outerHTML || ''
-}
 
 export async function mergePDFs(document: Buffer, toc: Buffer): Promise<Buffer> {
   const docuPDF = await PDFDocument.load(document)
   const tocPDF = await PDFDocument.load(toc)
-  const indices = tocPDF.getPages().map((page, index) => {
+  const indices = tocPDF.getPages().map((page: any, index: any) => {
     docuPDF.removePage(0)
     return index
   })
 
   const pages = await docuPDF.copyPages(tocPDF, indices)
-  pages.forEach((page, index) => docuPDF.insertPage(index, page))
+  pages.forEach((page: any, index: any) => docuPDF.insertPage(index, page))
   const data = await docuPDF.save()
   return Buffer.from(data)
 }
 
 async function run_test() {
-  console.log(run_test.name)
+  console.log(run_test.name);
 
+  const options: Config = {
+    input: '',
+    output: '',
+    style: '',
+    tocHeading: '',
+    format: 'A4' as PaperFormat,
+    scale: 1,
+    hMargin: '',
+    vMargin: '',
+    prismTheme: 'default',
+    mermaidTheme: 'default',
+    svgo: false,
+    numberSections: false,
+    shiftHeadingLevelBy: -1,
+    displayHeaderFooter: false,
+    headerTemplate: '',
+    footerTemplate: '',
+    chromePath: '',
+    savePng: false,
+    saveHtml: false,
+  };
+
+  const read_data = Deno.readFileSync(path.resolve(Deno.cwd(), '../pdf-generator-service/src/util/__test__/sample.pdf'));
+  const pdfBuffer = Buffer.from(read_data);
+  console.log(pdfBuffer);
+
+  const content = '';
+  const tocContext = { _toc: [] };
+  const context = { name: 'PDF Express' };
+  const tocTemplate = '';
+  await extractPDFToc(pdfBuffer, options, content, tocContext, context, tocTemplate);
+  console.log(content);
 }
 
 if (import.meta.main) {
